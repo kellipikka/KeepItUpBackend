4fef813da1daedad27aadedbab605ee6d673ab2c
Kelli Pikka <148441396+kellipikka@users.noreply.github.com>
2025-01-23 16:39:28 +0200

    Initial commit

---
+++ src/main/java/ee/kpikka/keepitupbackend/KeepItUpBackendApplication.java
@@ -0,0 +1,13 @@
+package ee.kpikka.keepitupbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KeepItUpBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KeepItUpBackendApplication.class, args);
+    }
+
+}
